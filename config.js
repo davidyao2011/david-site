84e3d7fa5477def45a04c8357b78f62cfe8c6be5
David Yao <davidyao2011@hotmail.com>
2020-01-09 11:49:52 +1300

    commint

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Prologue', // <title>
-  manifestName: 'Prologue',
+  siteTitle: 'David Site', // <title>
+  manifestName: 'David',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-prologue/`, // This path is subpath of your hosting https://domain/portfolio
-  authorName: 'Anubhav',
+  pathPrefix: `/david-site/`, // This path is subpath of your hosting https://domain/portfolio
+  authorName: 'David',
   heading: 'Web Developer',
   // social
   socialLinks: [
